bcdfa6eca289d823ebb00f627d1986dd71afe6a3
Serghey <serii.hmeli123123@mail.ru>
2020-06-28 22:03:12 +0300

    Updates

---
+++ precache-manifest.2937fe77f1c284b0d1def93b0b56cb54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d442abde8ed8d7696febbaffd520afd",
+    "url": "/Xyrolle/Portfolio/index.html"
+  },
+  {
+    "revision": "d02f65bdb2972d521f91",
+    "url": "/Xyrolle/Portfolio/static/css/main.70a8f1b2.chunk.css"
+  },
+  {
+    "revision": "d4e67a5dd768be8f9db1",
+    "url": "/Xyrolle/Portfolio/static/js/2.d6f17e89.chunk.js"
+  },
+  {
+    "revision": "3f2e81500fd00a4763a1368a012f51a9",
+    "url": "/Xyrolle/Portfolio/static/js/2.d6f17e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02f65bdb2972d521f91",
+    "url": "/Xyrolle/Portfolio/static/js/main.ce037497.chunk.js"
+  },
+  {
+    "revision": "faa9cb5aa2e35f8d6e2f",
+    "url": "/Xyrolle/Portfolio/static/js/runtime-main.701209a7.js"
+  }
+]);
