22ac83bc7cb21ee0365f3065ae346e17f3d139de
Serghey <serii.hmeli123123@mail.ru>
2020-06-28 22:11:34 +0300

    Updates

---
+++ precache-manifest.3ed731621dec8b57aab321b0e9f87af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b793bcd6b2df8c67981f68ff42cbe70",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "eb72dea2c9c01cb68c82",
+    "url": "/Portfolio/static/css/main.70a8f1b2.chunk.css"
+  },
+  {
+    "revision": "d4e67a5dd768be8f9db1",
+    "url": "/Portfolio/static/js/2.d6f17e89.chunk.js"
+  },
+  {
+    "revision": "3f2e81500fd00a4763a1368a012f51a9",
+    "url": "/Portfolio/static/js/2.d6f17e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb72dea2c9c01cb68c82",
+    "url": "/Portfolio/static/js/main.f19b6c2a.chunk.js"
+  },
+  {
+    "revision": "06d13b8f7356d3f59b33",
+    "url": "/Portfolio/static/js/runtime-main.aba7c214.js"
+  }
+]);
